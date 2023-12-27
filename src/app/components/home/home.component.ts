75b2acbf69ff2978e444518d69b86349dbecb4b4
Anna Hrulckova <annahrulkova@yandex.ru>
2023-12-27 17:59:42 +0400

    базовые стили, разметка

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,9 +3,10 @@ import { NavigationBarComponent } from '../navigation-bar/navigation-bar.compone
 import { DashboardComponent } from '../dashboard/dashboard.component';
 @Component({
   selector: 'app-home',
+  
   standalone: true,
   imports: [NavigationBarComponent, DashboardComponent],
   templateUrl: './home.component.html',
-  styleUrl:'./home.component.css',
+  styleUrls: ['./home.component.css'] ,
 })
 export class HomeComponent {}
